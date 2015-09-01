8f8b85058478295109bafd61128c74e7c5ae9b75
Toine Hartman <tjbhartman@gmail.com>
2015-09-01 12:38:43 +0200

    Added sample JavaFX code.
    Use to understand view-controller relationship in JavaFX.

---
+++ src/main/java/BubbleBobble.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BubbleBobble extends Application {
+
+    /**
+     * The main method just launches the application.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * The start method sets up the application window.
+     *
+     * The view is loaded from an FXML file. A title for the window is set.
+     * The loaded view is set as the current scene.
+     *
+     * @param primaryStage The primary stage (window).
+     * @throws IOException When the FXML file is not found.
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("BubbleBobble");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
