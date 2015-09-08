a44b553021e786290cbf412847a1085b3f080b9e
jimhommes <jim@homm.es>
2015-09-08 09:29:14 +0200

    Added Monster class

---
+++ src/main/java/model/Monster.java
@@ -0,0 +1,36 @@
+package model;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+
+/**
+ * Created by Jim on 9/8/2015.
+ *
+ * @author Jim
+ * @version 0.1
+ * @since 9/8/2015
+ */
+public class Monster extends SpriteBase {
+
+    private final double speed;
+    private final boolean facingRight;
+
+    public Monster(Pane layer, Image image, double x, double y, double r,
+                   double dx, double dy, double dr, double speed, boolean facingRight) {
+        super(layer,image,x,y,r,dx,dy,dr);
+
+        this.speed = speed;
+        this.facingRight = facingRight;
+    }
+
+    public void move() {
+        if (facingRight) {
+            dx += speed;
+        } else {
+            dx -= speed;
+        }
+
+        super.move();
+    }
+
+}
