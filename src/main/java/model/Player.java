e9d8c54fcad2740a9477f2242e20f37aaf6cbd93
jimhommes <jim@homm.es>
2015-09-10 11:33:40 +0200

    Function for collision added

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -190,5 +190,24 @@ public class Player extends GravityObject {
         return bubbles;
     }
 
+    public void checkCollideMonster(final Monster monster) {
+        double monsterX = monster.getX();
+        double monsterMaxX = monsterX + monster.getImage().getWidth();
+        double monsterY = monster.getY();
+        double monsterMaxY = monsterY + monster.getImage().getHeight();
+
+        if((monsterX > x && monsterX < x + image.getWidth()) ||
+                (monsterMaxX > x && monsterMaxX < x + image.getWidth())) {
+            if((monsterY > y && monsterY < y + image.getHeight()) ||
+                    (monsterMaxY > y && monsterMaxX < y + image.getHeight())) {
+                die();
+            }
+        }
+    }
+
+    public void die() {
+        image = new Image(getClass().getResource("/BubbleBobbleLogo.png").toExternalForm());
+    }
+
 
 }
