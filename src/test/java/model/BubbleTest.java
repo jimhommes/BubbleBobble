04b3f3f8e6cbde81495ba2b55ca4e116e0a7faef
Toine Hartman <tjbhartman@gmail.com>
2015-09-11 14:21:50 +0200

    Test Bubbles fired to the left.

--- src/test/java/model/BubbleTest.java
+++ src/test/java/model/BubbleTest.java
@@ -6,29 +6,46 @@ import junit.framework.TestCase;
  * Created by toinehartman on 11/09/15.
  */
 public class BubbleTest extends TestCase {
-    private static Bubble bubble;
+    private static Bubble bubbleRight;
+    private static Bubble bubbleLeft;
 
     public void setUp() {
-        bubble = new Bubble(1, 1, 0, 0, 0, 0, true);
+        bubbleRight = new Bubble(1, 1, 0, 0, 0, 0, true);
+        bubbleLeft = new Bubble(1, 1, 0, 0, 0, 0, false);
     }
 
-    public void testMove() {
-        assertTrue(bubble.getAbleToCatch());
+    public void testMoveRight() {
+        assertTrue(bubbleRight.getAbleToCatch());
 
         for (int i = 1; i <= 30; i++) {
-            bubble.move();
+            bubbleRight.move();
 
-            assertEquals((double) 1.f + i * 7, bubble.getX());
-            assertEquals((double) 1.f, bubble.getY());
+            assertEquals((double) 1.f + i * 7, bubbleRight.getX());
+            assertEquals((double) 1.f, bubbleRight.getY());
         }
 
-        assertTrue(bubble.getAbleToCatch());
-        bubble.move();
-        assertFalse(bubble.getAbleToCatch());
+        assertTrue(bubbleRight.getAbleToCatch());
+        bubbleRight.move();
+        assertFalse(bubbleRight.getAbleToCatch());
+    }
+
+    public void testMoveLeft() {
+        assertTrue(bubbleLeft.getAbleToCatch());
+
+        for (int i = 1; i <= 30; i++) {
+            bubbleLeft.move();
+
+            assertEquals((double) 1.f + i * -7, bubbleLeft.getX());
+            assertEquals((double) 1.f, bubbleLeft.getY());
+        }
+
+        assertTrue(bubbleLeft.getAbleToCatch());
+        bubbleLeft.move();
+        assertFalse(bubbleLeft.getAbleToCatch());
     }
 
     public void testSetAbleToCatch() {
-        bubble.setAbleToCatch(false);
-        assertFalse(bubble.getAbleToCatch());
+        bubbleRight.setAbleToCatch(false);
+        assertFalse(bubbleRight.getAbleToCatch());
     }
 }
