9e5a915bcf5e564222dffc7526e118cc15720403
jimhommes <jim@homm.es>
2015-09-11 14:18:13 +0200

    Added player tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,14 +1,14 @@
-package test;
-
 import controller.LevelController;
+import controller.ScreenController;
 import junit.framework.TestCase;
 import model.Input;
 import model.Monster;
 import model.Player;
 import model.Settings;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Jim on 9/11/2015.
@@ -21,14 +21,19 @@ public class PlayerTest extends TestCase {
 
     private static Player player;
     private static Input input;
+    private static LevelController levelController;
+    private static ScreenController screenController;
 
+    @Before
     public void setUp() throws Exception {
         input = mock(Input.class);
-        LevelController levelController = mock(LevelController.class);
+        levelController = mock(LevelController.class);
+        screenController = mock(ScreenController.class);
         player = new Player(0, 0, 0, 0, 0, 0, Settings.PLAYER_SPEED, input, levelController);
     }
 
-    public void testProcessInput() throws Exception {
+    @Test
+    public void testProcessInputNotDead() throws Exception {
         when(input.isMoveDown()).thenReturn(true);
         when(input.isMoveLeft()).thenReturn(true);
         assertEquals(0.0, player.getDy());
@@ -40,6 +45,17 @@ public class PlayerTest extends TestCase {
         assertEquals(0.0, player.getY());
     }
 
+    @Test
+    public void testProcessInputDead() throws Exception {
+        player.die();
+        for(int i = 0; i < 100; i++) {
+            player.processInput();
+        }
+        verify(levelController, atLeastOnce()).gameOver();
+        assertTrue(player.getGameOver());
+    }
+
+    @Test
     public void testMove() throws Exception {
         when(input.isMoveDown()).thenReturn(true);
         when(input.isMoveLeft()).thenReturn(true);
@@ -49,28 +65,92 @@ public class PlayerTest extends TestCase {
         assertEquals(Settings.PLAYER_SPEED - player.calculateGravity(), player.getY());
     }
 
+    @Test
     public void testGetBubbles() throws Exception {
         when(input.isFirePrimaryWeapon()).thenReturn(true);
+        when(levelController.getScreenController()).thenReturn(screenController);
         assertTrue(player.getBubbles().size() == 0);
         player.processInput();
         assertTrue(player.getBubbles().size() > 0);
     }
 
+    @Test
     public void testCheckCollideMonster() throws Exception {
         Monster monster = mock(Monster.class);
+        player.setWidth(100);
+        player.setHeight(100);
         when(monster.getX()).thenReturn(1.0);
         when(monster.getY()).thenReturn(1.0);
         when(monster.getWidth()).thenReturn(300.0);
         when(monster.getHeight()).thenReturn(300.0);
         player.checkCollideMonster(monster);
-//        assertTrue(player.getDead());
+        assertTrue(player.getDead());
     }
 
+    @Test
     public void testDie() throws Exception {
         assertFalse(player.getDead());
         player.die();
         assertTrue(player.getDead());
         assertTrue(player.getImagePath().equals("/BubbleBobbleLogo.png"));
+    }
+
+    @Test
+    public void testMoveRight() throws Exception {
+        when(input.isMoveRight()).thenReturn(true);
+        assertEquals(0.0, player.getX());
+        player.processInput();
+        player.move();
+        assertEquals(Settings.PLAYER_SPEED, player.getX());
+    }
+
+    @Test
+    public void testCollisionRight() throws Exception {
+        when(input.isMoveRight()).thenReturn(true);
+        when(levelController.causesCollision(any(Double.class),
+                any(Double.class),
+                any(Double.class),
+                any(Double.class))).thenReturn(true);
+        player.processInput();
+        player.move();
+        assertEquals(0.0, player.getX());
+    }
+
+    @Test
+    public void testCollisionLeft() throws Exception {
+        when(input.isMoveLeft()).thenReturn(true);
+        when(levelController.causesCollision(any(Double.class),
+                any(Double.class),
+                any(Double.class),
+                any(Double.class))).thenReturn(true);
+
+        player.processInput();
+        player.move();
+        assertEquals(0.0, player.getX());
+    }
+
+    @Test
+    public void testMoveUp() throws Exception {
+        when(input.isMoveUp()).thenReturn(true);
+        player.processInput();
+        player.move();
+
+        assertEquals(-Settings.PLAYER_SPEED - player.calculateGravity(), player.getY());
+        assertEquals(0.0, player.getX());
+    }
+
+    @Test
+    public void testCollisionUp() throws Exception {
+        when(input.isMoveUp()).thenReturn(true);
+        when(levelController.causesCollision(any(Double.class),
+                any(Double.class),
+                any(Double.class),
+                any(Double.class))).thenReturn(true);
+        player.processInput();
+        player.move();
+
+        //Gravity also doesn't work if for all doubles there is a collision
+        assertEquals(0.0, player.getY());
     }
 
 }
