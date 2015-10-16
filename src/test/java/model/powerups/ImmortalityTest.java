7589b568038ce31a830aecaf856b1fbc75943421
Toine Hartman <tjbhartman@gmail.com>
2015-10-16 12:25:07 +0200

    Added tests for refactored PlayerEnhancements.
    < testing

---
+++ src/test/java/model/powerups/ImmortalityTest.java
@@ -0,0 +1,45 @@
+package model.powerups;
+
+import model.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Tests the bubbles.
+ */
+public class ImmortalityTest {
+
+    PlayerEnhancement playerEnhancement;
+    Player p = mock(Player.class);
+
+    /**
+     * This method is run before all the tests to initialize them.
+     */
+    @Before
+    public void setUp() {
+        playerEnhancement = spy(new Immortality(p));
+    }
+
+    @Test
+    public void checkTest() {
+        boolean result = playerEnhancement.check();
+        int counter = 1;
+
+        while (result) {
+            assertTrue(result);
+            counter++;
+
+            result = playerEnhancement.check();
+        }
+
+        assertEquals(350, counter);
+        verify(playerEnhancement, times(1)).remove();
+    }
+}
