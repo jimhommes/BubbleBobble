cb163d759a2ef88e45729199fff1e6c439831cff
Toine Hartman <tjbhartman@gmail.com>
2015-10-16 20:25:29 +0200

    Fixed feature envy.
    < organisation

--- src/main/java/model/Coordinates.java
+++ src/main/java/model/Coordinates.java
@@ -31,6 +31,12 @@ public class Coordinates {
         this.dy = dy;
         this.dr = dr;
 	}
+
+	public void apply() {
+		this.setX(x + dx);
+		this.setY(y + dy);
+		this.setR(r + dr);
+	}
 	
 	/**
 	 * This method gets the x of the coordinates. 
