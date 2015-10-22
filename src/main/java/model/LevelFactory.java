e827f3cebbd2fa77a29ad25a3d1573d6e8039341
jimhommes <jim@homm.es>
2015-10-22 22:02:10 +0200

    Added a LevelFactory.
    <organisation

---
+++ src/main/java/model/LevelFactory.java
@@ -0,0 +1,103 @@
+package model;
+
+import controller.LevelController;
+import utility.Logger;
+import utility.Settings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Jim on 10/22/2015.
+ *
+ * @author Jim
+ * @version 1.0
+ * @since 10/22/2015
+ */
+public class LevelFactory {
+
+    private static final int NUM_ROWS = 26;
+    private static final int NUM_COLS = 26;
+    public static final double SPRITE_SIZE = 32.0;
+
+    /**
+     * The map in a 2 dim array.
+     */
+    private Integer[][] map;
+
+    private final LevelController levelController;
+
+    public LevelFactory(final LevelController levelController) {
+        this.levelController = levelController;
+    }
+
+    public Level makeLevel(String levelTitle, int limitOfPlayers) {
+        Level level = new Level(levelController);
+        readMap(levelTitle);
+        drawMap(level, limitOfPlayers);
+        return level;
+    }
+
+    /**
+     * The function that draws the map.
+     */
+    public final void drawMap(Level level, int limitOfPlayers) {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                Coordinates coordinatesWalker = new Coordinates(col * SPRITE_SIZE - 32,
+                        row * SPRITE_SIZE - 32, 0, 0, 0, 0);
+                if (map[row][col] == 1) {
+                    Coordinates coordinatesWall =
+                            new Coordinates(col * SPRITE_SIZE, row * SPRITE_SIZE, 0, 0, 0, 0);
+                    level.addWall(new Wall(coordinatesWall));
+                } else if (map[row][col] == 2) {
+                    level.addMonster(new Walker(coordinatesWalker,
+                            Settings.MONSTER_SPEED, true, levelController));
+                } else if (map[row][col] == 3) {
+                    level.addMonster(new Walker(coordinatesWalker,
+                            Settings.MONSTER_SPEED, false, levelController));
+                } else if (map[row][col] == 9) {
+                    Logger.log(String.format("Player found in %d, %d%n", row, col));
+                    int playerCounter = level.getPlayers().size();
+                    if (playerCounter < limitOfPlayers) {
+                        Coordinates coordinatesPlayer = new Coordinates(col * SPRITE_SIZE - 32,
+                                row * SPRITE_SIZE - 32, 0, 0, 0, 0);
+                        level.addPlayer(new Player(levelController, coordinatesPlayer,
+                                Settings.PLAYER_SPEED, Settings.PLAYER_LIVES, null, playerCounter));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * This function reads the file and translates it to a 2dim array.
+     */
+    private void readMap(String levelTitle) {
+        int row = 0;
+        map = new Integer[NUM_ROWS][NUM_COLS];
+        BufferedReader reader;
+
+        try (InputStreamReader isr = new InputStreamReader(getClass()
+                .getClassLoader()
+                .getResourceAsStream(levelTitle), "UTF-8")) {
+            reader = new BufferedReader(isr);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] cols = line.split(" ");
+                if (cols.length == NUM_COLS) {
+                    for (int column = 0; column < cols.length; column++) {
+                        map[row][column] = Integer.parseInt(cols[column]);
+                    }
+                }
+                row++;
+            }
+            reader.close();
+            isr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
